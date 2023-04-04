a12a45c5346ac9e5c75771e4a8c9a24ef3bdede1
eder12324 <eder_rdz_mtz@hotmail.com>
2023-04-04 17:40:41 +0200

    ALH included

--- stats.py
+++ stats.py
@@ -4,6 +4,8 @@ import pickle
 # Load stat_df.pkl
 with open('stat_df.pkl', 'rb') as f:
     stat_df = pickle.load(f)
+stat_df['ALH'][3538] = 2.7122898 # Error in the input master dataframe
+stat_df['ALH'] = stat_df['ALH'].astype(float)
 
 # Types of elements in stat_df['exposure']
 exp_list = stat_df['exposure'].unique()
@@ -22,12 +24,10 @@ c8_labels = stat_df['c8_labels'].unique()
 
 c_list = [c6_labels, c7_labels, c8_labels] # implement later
 
-mot_param = ['VCL', 'VAP', 'VSL', 'LIN', 'STR', 'WOB', 'BeatCross']#, 'ALH']
-
-# Standard error
+mot_param = ['VCL', 'VAP', 'VSL', 'LIN', 'STR', 'WOB', 'BeatCross', 'ALH']
 
 print('num_c = 6')
-# Calculate mean and standard deviation of mot_param for each element in exp_list, qty_list and c6_labels
+# Calculate mean and standard error of mot_param for each element in exp_list, qty_list and c6_labels
 for exp in exp_list:
     for qty in qty_list:
         for c6 in c6_labels:
@@ -40,7 +40,7 @@ for exp in exp_list:
 
 
 print('num_c = 7')
-# Calculate mean and standard deviation of mot_param for each element in exp_list, qty_list and c7_labels
+# Calculate mean and standard error of mot_param for each element in exp_list, qty_list and c7_labels
 for exp in exp_list:
     for qty in qty_list:
         for c7 in c7_labels:
@@ -52,7 +52,7 @@ for exp in exp_list:
                 print()
 
 print('num_c = 8')
-# Calculate mean and standard deviation of mot_param for each element in exp_list, qty_list and c8_labels
+# Calculate mean and standard error of mot_param for each element in exp_list, qty_list and c8_labels
 for exp in exp_list:
     for qty in qty_list:
         for c8 in c8_labels:
