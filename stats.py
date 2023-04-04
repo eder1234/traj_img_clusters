63793c068a8bf758f0c144272dc6714798281017
eder12324 <eder_rdz_mtz@hotmail.com>
2023-04-04 16:53:33 +0200

    standard error

--- stats.py
+++ stats.py
@@ -24,6 +24,8 @@ c_list = [c6_labels, c7_labels, c8_labels] # implement later
 
 mot_param = ['VCL', 'VAP', 'VSL', 'LIN', 'STR', 'WOB', 'BeatCross']#, 'ALH']
 
+# Standard error
+
 print('num_c = 6')
 # Calculate mean and standard deviation of mot_param for each element in exp_list, qty_list and c6_labels
 for exp in exp_list:
@@ -31,9 +33,11 @@ for exp in exp_list:
         for c6 in c6_labels:
             for param in mot_param:
                 print('exp =', exp, 'qty =', qty, 'c6 =', c6, 'param =', param)
-                print('mean =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c6)][param].mean())
-                print('std =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c6)][param].std())
+                describe = stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c6)][param].describe()
+                print(describe)
+                print('Standard error =', describe['std']/describe['count']**0.5)
                 print()
+
 
 print('num_c = 7')
 # Calculate mean and standard deviation of mot_param for each element in exp_list, qty_list and c7_labels
@@ -42,8 +46,9 @@ for exp in exp_list:
         for c7 in c7_labels:
             for param in mot_param:
                 print('exp =', exp, 'qty =', qty, 'c7 =', c7, 'param =', param)
-                print('mean =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c7)][param].mean())
-                print('std =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c7)][param].std())
+                describe = stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c7_labels'] == c7)][param].describe()
+                print(describe)
+                print('Standard error =', describe['std']/describe['count']**0.5)
                 print()
 
 print('num_c = 8')
@@ -53,8 +58,10 @@ for exp in exp_list:
         for c8 in c8_labels:
             for param in mot_param:
                 print('exp =', exp, 'qty =', qty, 'c8 =', c8, 'param =', param)
-                print('mean =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c8)][param].mean())
-                print('std =', stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c6_labels'] == c8)][param].std())
+                describe = stat_df[(stat_df['exposure'] == exp) & (stat_df['quantity'] == qty) & (stat_df['c8_labels'] == c8)][param].describe()
+                print(describe)
+                print('Standard error =', describe['std']/describe['count']**0.5)
                 print()
+
 
 print('Done')
